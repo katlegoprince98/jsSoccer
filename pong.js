8069cb510f7eac033a3000da7f0c5d69480d84c2
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-11 17:21:17 +0200

    258

--- pong.js
+++ pong.js
@@ -3,7 +3,7 @@
 const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext('2d');
 
-function drawRect(x,y,w,h) {
+function drawRect(x,y,w,h, color) {
     context.fillStyle = color;
     context.fillRect(x,y,w,h);  
 }
