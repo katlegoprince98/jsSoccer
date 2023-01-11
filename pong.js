11b1ee6ff9ab8a1497db3cc3b395f66938a5b8f6
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-11 17:18:19 +0200

    5

--- pong.js
+++ pong.js
@@ -1,3 +1,8 @@
 //Pull the id the of the canvas
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('myCanvas');
+const context = canvas.getContext('2d');
+
+function drawRect(){
+        
+}
